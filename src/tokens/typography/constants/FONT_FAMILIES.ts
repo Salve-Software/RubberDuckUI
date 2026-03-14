859fa997a27677bc70efbab3fea45dc3edb8d6be
eumaninho54 <angelo.omarzinho@outlook.com>
2026-03-14 17:43:47 -0300

    feat: create Typography class to config

---
+++ src/tokens/typography/constants/FONT_FAMILIES.ts
@@ -0,0 +1,11 @@
+export const FONT_FAMILIES = {
+  sans: {
+    regular: 'Geist-Regular',
+    medium: 'Geist-Medium',
+    semibold: 'Geist-SemiBold',
+    bold: 'Geist-Bold',
+  },
+  mono: {
+    regular: 'GeistMono-Regular',
+  },
+} as const;
