400cbc3414cc32843731ba2006f71e4ef1ac4668
eumaninho54 <angelo.omarzinho@outlook.com>
2026-03-14 19:48:21 -0300

    chore: remove prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,6 @@
 import { fixupConfigRules } from '@eslint/compat';
 import { FlatCompat } from '@eslint/eslintrc';
 import js from '@eslint/js';
-import prettier from 'eslint-plugin-prettier';
 import { defineConfig } from 'eslint/config';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
@@ -18,11 +17,14 @@ const compat = new FlatCompat({
 export default defineConfig([
   {
     extends: fixupConfigRules(compat.extends('@react-native', 'prettier')),
-    plugins: { prettier },
     rules: {
       'react/react-in-jsx-scope': 'off',
-
-      'prettier/prettier': 'error',
+      'object-curly-newline': [
+        'error',
+        {
+          ObjectPattern: { minProperties: 4 },
+        },
+      ],
     },
   },
   {
