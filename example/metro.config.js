8aa33a0aad3e11b563306dc9a3f8fc4321fcc8de
eumaninho54 <angelo.omarzinho@outlook.com>
2026-03-15 17:58:40 -0300

    feat: configure storybook on example app

--- example/metro.config.js
+++ example/metro.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const { getDefaultConfig } = require('@expo/metro-config');
 const { withMetroConfig } = require('react-native-monorepo-config');
+const { withStorybook } = require('@storybook/react-native/metro/withStorybook');
 
 const root = path.resolve(__dirname, '..');
 
@@ -15,4 +16,7 @@ const config = withMetroConfig(getDefaultConfig(__dirname), {
   dirname: __dirname,
 });
 
-module.exports = config;
+module.exports = withStorybook(config, {
+  enabled: true,
+  configPath: path.resolve(__dirname, './.rnstorybook'),
+});
